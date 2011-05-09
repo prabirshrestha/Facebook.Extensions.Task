7be75a0db330dbd3ac154f7ab6b7b3fbadcb386f
prabirshrestha <mail@prabir.me>
2011-05-09 21:44:26 +0545

    + CSNet40 TPL Sample

---
+++ samples/CSNet40TPLSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSNet40TPLSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
