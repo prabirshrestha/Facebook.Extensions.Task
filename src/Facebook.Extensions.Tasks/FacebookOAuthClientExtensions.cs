e248a8a86f25c8d5cdb566318261b6fd3cbc343a
prabirshrestha <mail@prabir.me>
2011-05-09 20:02:00 +0545

    + FacebookOAuthClientExtensions

---
+++ src/Facebook.Extensions.Tasks/FacebookOAuthClientExtensions.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Facebook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics.Contracts;
+    using System.Threading.Tasks;
+    using Facebook;
+
+    public static class FacebookOAuthClientExtensions
+    {
+    }
+}
