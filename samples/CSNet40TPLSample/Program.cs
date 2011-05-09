4ba0b3bd264e6b5af90ae2d50c6f8350ee22d548
prabirshrestha <mail@prabir.me>
2011-05-09 22:06:19 +0545

    + example using TPL

--- samples/CSNet40TPLSample/Program.cs
+++ samples/CSNet40TPLSample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Facebook;
 
 namespace CSNet40TPLSample
 {
@@ -9,6 +10,29 @@ namespace CSNet40TPLSample
     {
         static void Main(string[] args)
         {
+            ExecuteAsyncMethod();            
+
+            Console.ReadKey();
+        }
+
+        private static void ExecuteAsyncMethod()
+        {
+            var fb = new FacebookClient();
+            var task = fb.GetTaskAsync("/4");
+
+            task.ContinueWith(
+                t =>
+                {
+                    if (t.Exception == null)
+                    {
+                        dynamic result = t.Result;
+                        Console.WriteLine("Hi {0}", result.name);
+                    }
+                    else
+                    {
+                        Console.WriteLine("error occurred");
+                    }
+                });
         }
     }
 }
