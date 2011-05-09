b7edbcd3c1b2268463cde460eff3c9e8cd185fbd
prabirshrestha <mail@prabir.me>
2011-05-09 19:42:09 +0545

    + Get,Post,Delete TaskAsync methods

--- src/Facebook.Extensions.Tasks/FacebookTaskExtensions.cs
+++ src/Facebook.Extensions.Tasks/FacebookTaskExtensions.cs
@@ -1,11 +1,167 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace Facebook
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Diagnostics.Contracts;
+    using System.Threading.Tasks;
+    using Facebook;
+
     public static class FacebookTaskExtensions
     {
+        public static Task<object> DeleteTaskAsync(this FacebookClient facebookClient, string path)
+        {
+            return DeleteTaskAsync(facebookClient, path, null);
+        }
+
+        public static Task<object> DeleteTaskAsync(this FacebookClient facebookClient, string path, IDictionary<string, object> parameters)
+        {
+            Contract.Requires(!(string.IsNullOrEmpty(path) && parameters == null));
+
+            var tcs = CreateSource<object>(null);
+
+            EventHandler<FacebookApiEventArgs> handler = null;
+            handler = (sender, e) => TransferCompletionToTask<object>(tcs, e, () => e.GetResultData(), () => facebookClient.DeleteCompleted -= handler);
+
+            facebookClient.DeleteCompleted += handler;
+
+            try
+            {
+                facebookClient.DeleteAsync(path, parameters, tcs);
+            }
+            catch
+            {
+                facebookClient.DeleteCompleted -= handler;
+                tcs.TrySetCanceled();
+                throw;
+            }
+
+            return tcs.Task;
+        }
+
+        public static Task<object> GetTaskAsync(this FacebookClient facebookClient, string path)
+        {
+            Contract.Requires(!string.IsNullOrEmpty(path));
+
+            return GetTaskAsync(facebookClient, path, null);
+        }
+
+        public static Task<object> GetTaskAsync(this FacebookClient facebookClient, string path, IDictionary<string, object> parameters)
+        {
+            Contract.Requires(!(string.IsNullOrEmpty(path) && parameters == null));
+
+            var tcs = CreateSource<object>(null);
+
+            EventHandler<FacebookApiEventArgs> handler = null;
+            handler = (sender, e) => TransferCompletionToTask<object>(tcs, e, () => e.GetResultData(), () => facebookClient.GetCompleted -= handler);
+
+            facebookClient.GetCompleted += handler;
+
+            try
+            {
+                facebookClient.GetAsync(path, parameters, tcs);
+            }
+            catch
+            {
+                facebookClient.GetCompleted -= handler;
+                tcs.TrySetCanceled();
+                throw;
+            }
+
+            return tcs.Task;
+        }
+
+        public static Task<object> GetTaskAsync(this FacebookClient facebookClient, IDictionary<string, object> parameters)
+        {
+            Contract.Requires(parameters != null);
+
+            return GetTaskAsync(facebookClient, null, parameters);
+        }
+
+        public static Task<object> PostTaskAsync(this FacebookClient facebookClient, string path, IDictionary<string, object> parameters)
+        {
+            Contract.Requires(!(string.IsNullOrEmpty(path) && parameters == null));
+
+            var tcs = CreateSource<object>(null);
+
+            EventHandler<FacebookApiEventArgs> handler = null;
+            handler = (sender, e) => TransferCompletionToTask<object>(tcs, e, () => e.GetResultData(), () => facebookClient.PostCompleted -= handler);
+
+            facebookClient.PostCompleted += handler;
+
+            try
+            {
+                facebookClient.PostAsync(path, parameters, tcs);
+            }
+            catch
+            {
+                facebookClient.PostCompleted -= handler;
+                tcs.TrySetCanceled();
+                throw;
+            }
+
+            return tcs.Task;
+        }
+
+        public static Task<object> PostTaskAsync(this FacebookClient facebookClient, IDictionary<string, object> parameters)
+        {
+            Contract.Requires(parameters != null);
+
+            return PostTaskAsync(facebookClient, null, parameters);
+        }
+
+        public static Task<object> PostTaskAsync(this FacebookClient facebookClient, string path, object parameters)
+        {
+            Contract.Requires(!(string.IsNullOrEmpty(path) && parameters == null));
+
+            return PostTaskAsync(facebookClient, path, ToDictionary(parameters));
+        }
+
+        public static Task<object> PostTaskAsync(this FacebookClient facebookClient, object parameters)
+        {
+            Contract.Requires(parameters != null);
+
+            return PostTaskAsync(facebookClient, null, parameters);
+        }
+
+        private static TaskCompletionSource<T> CreateSource<T>(object state)
+        {
+            return new TaskCompletionSource<T>(state);
+        }
+
+        private static void TransferCompletionToTask<T>(TaskCompletionSource<T> tcs, AsyncCompletedEventArgs e, Func<T> getResult, Action unregisterHandler)
+        {
+            if (e.UserState == tcs)
+            {
+                if (e.Cancelled) tcs.TrySetCanceled();
+                else if (e.Error != null) tcs.TrySetException(e.Error);
+                else tcs.TrySetResult(getResult());
+                if (unregisterHandler != null) unregisterHandler();
+            }
+        }
+
+        /// <summary>
+        /// Convert the object to dictionary.
+        /// </summary>
+        /// <param name="parameters">
+        /// The parameters.
+        /// </param>
+        /// <returns>
+        /// Returns the dictionary equivalent of the specified object.
+        /// </returns>
+        private static IDictionary<string, object> ToDictionary(object parameters)
+        {
+            Contract.Requires(parameters != null);
+            Contract.Ensures(Contract.Result<IDictionary<string, object>>() != null);
+
+            if (parameters is JsonObject)
+            {
+                return (JsonObject)parameters;
+            }
+
+            var json = JsonSerializer.Current.SerializeObject(parameters);
+            return (IDictionary<string, object>)JsonSerializer.Current.DeserializeObject(json);
+        }
     }
 }
